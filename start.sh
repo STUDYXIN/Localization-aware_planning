a5daad9f8b2d75c6faee19d4f1a9739085d8b828
STUDYXIN <782848000@qq.com>
2024-08-20 23:52:39 +0800

    生成了地图，可以在新地图上跑fuel

--- start.sh
+++ start.sh
@@ -5,16 +5,21 @@ FILEDIR=$(readlink -f "${BASH_SOURCE[0]}")
 BASEDIR=$(dirname "${FILEDIR}")
 SETUP_FILE="${BASEDIR}/../../devel/setup.bash"
 SETUP_DIR=$(dirname "${SETUP_FILE}")
+PLY_NAME=right_corner.ply
 
 echo "setup.bash is located in: ${SETUP_DIR}"
 # 加载 ROS 环境
 source "${SETUP_FILE}"
-
+# 启动 Rviz
 sleep 0.5s
 gnome-terminal --tab -- bash -c "\
 echo Rviz; \
 roslaunch exploration_manager rviz.launch; exec bash"
-
+# 加载点云文件
+sleep 1s
+gnome-terminal --tab -- bash -c "\
+echo PCL; \
+roslaunch map_publisher map_publisher.launch file_name:=${PLY_NAME}; exec bash"
 # 启动 Planner
 sleep 1s
 gnome-terminal --tab -- bash -c "\
